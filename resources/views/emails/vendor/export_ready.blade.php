20ed7264a489a39bd1957e000c5a92d402fa0b5e
Omar Mohamed <aboutsystem2@gmail.com>
2025-10-22 10:21:39 +0300

    Fresh upload of all system files for complete Codacy analysis

--- resources/views/emails/vendor/export_ready.blade.php
+++
@@ -1,4 +0,0 @@
-<p>Hello,</p>
-<p>Your requested vendor orders export is ready. You can download it using the link below (valid for 2 hours):</p>
-<p><a href="{{ $url }}">Download export ({{ $filename }})</a></p>
-<p>If you did not request this export, ignore this message.</p>
