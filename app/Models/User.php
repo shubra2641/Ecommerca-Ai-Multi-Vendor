50c2c2177f600f77b1575efeac87a623dc021618
Omar Mohamed <aboutsystem2@gmail.com>
2025-10-23 11:26:18 +0300

    Fix User model hidden property type definition

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable implements MustVerifyEmail
      *
      * @var list<string>
      */
-    protected array $hidden = [
+    protected $hidden = [
         'password',
         'remember_token',
     ];
